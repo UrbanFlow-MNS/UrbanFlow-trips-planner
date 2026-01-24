ba16d4a0a8efd6a51e36eafc7d5a2152ce8c85c0
Theo Sementa <theosementa@gmail.com>
2026-01-24 13:52:16 +0100

    feat: add queue and message

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller("trips-planner")
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get("fetchTrips") // TODO: Just for testing
+    async fetchTrips() {
+        return await this.appService.fetchAllTrips()
+    }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+
+    if (process.env.ENABLE_RABBITMQ === 'true') {
+        app.connectMicroservice<MicroserviceOptions>({
+            transport: Transport.RMQ,
+            options: {
+                urls: [process.env.RABBIT_MQ ?? ''],
+                queue: 'TRIPS_PLANNER_QUEUE',
+                queueOptions: {
+                    durable: false,
+                },
+            },
+        });
+
+        // For gateway
+        app.connectMicroservice<MicroserviceOptions>({
+            transport: Transport.TCP,
+            options: {
+                host: '0.0.0.0',
+                port: Number(process.env.TCP_PORT) || 0
+            },
+        });
+
+        await app.startAllMicroservices()
+    }
+
+    await app.listen(process.env.API_PORT ?? 0);
 }
 bootstrap();
