a10a302f429cfb72858e73bd7d896dba0000bcc0
Ethan-C11 <ethan.collignon.1102@gmail.com>
2026-02-19 15:31:54 +0100

    feat: ongoing C# migration & preparation

--- UrbanFlow-trips-planner/Program.cs
+++ UrbanFlow-trips-planner/Program.cs
@@ -1,3 +1,5 @@
+using UrbanFlow_trips_planner.API.GrpcServices;
+
 namespace UrbanFlow_trips_planner;
 
 public class Program
@@ -8,41 +10,22 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddAuthorization();
-
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
-
+        builder.Services.AddControllers();
+        builder.Services.AddGrpc();
+        
         var app = builder.Build();
+        
+        app.MapGrpcService<GreeterService>();
+        // juste pour tester le greeter
+        app.MapGet("/",
+            () =>
+                "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi();
-        }
 
         app.UseHttpsRedirection();
-
         app.UseAuthorization();
-
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                        new WeatherForecast
-                        {
-                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                            TemperatureC = Random.Shared.Next(-20, 55),
-                            Summary = summaries[Random.Shared.Next(summaries.Length)]
-                        })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
-
+        app.MapControllers();
+        
         app.Run();
     }
 }
