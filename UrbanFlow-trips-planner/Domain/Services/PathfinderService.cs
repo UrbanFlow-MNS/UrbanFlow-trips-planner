ae3dc24bcff40f9b7f58e9fc0763926c7c62cdd2
Ethan <ethan.collignon.1102@gmail.com>
2026-03-08 10:59:58 +0100

    feat: Service skeleton

---
+++ UrbanFlow-trips-planner/Domain/Services/PathfinderService.cs
@@ -0,0 +1,21 @@
+﻿using UrbanFlow_trips_planner.Domain.Entities;
+
+namespace UrbanFlow_trips_planner.Domain.Services;
+
+public class PathfinderService : IPathfinderService
+{
+    public Task<List<TripEntity>> GetTrips()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<TripEntity>> GetTripsBasedOnDistanceFromStartPosAndEndPos(float startLong, float startLat, float endLong, float endLat)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<TripEntity>> GetFastestRoutes(float startLong, float startLat, float endLong, float endLat)
+    {
+        throw new NotImplementedException();
+    }
+}
